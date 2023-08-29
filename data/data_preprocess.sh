e231181617a549adf4742be68b263318972c0fe4
AlexHodg <alexander.hodges@student.unsw.edu.au>
2023-08-29 15:59:49 +1000

    Update data_preprocess start report notebook

--- data/data_preprocess.sh
+++ data/data_preprocess.sh
@@ -4,4 +4,9 @@ cat forecastdemand_nsw.csv.zip* > ./forecastdemand_nsw.csv.zip
 
 unzip forecastdemand_nsw.csv.zip
 
+unzip temperature_nsw.csv.zip
+
+unzip totaldemand_nsw.csv.zip
+
 rm forecastdemand_nsw.csv.zip
+rm -rf __MACOSX
